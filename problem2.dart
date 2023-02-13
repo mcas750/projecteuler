5345ee2e895b39e16f1dd59cabaca29b09c6d52a
mcas750 <48414763+mcas750@users.noreply.github.com>
2023-02-13 22:06:48 +0300

    Update problem2.dart

--- problem2.dart
+++ problem2.dart
@@ -1,1 +1,13 @@
-
+void main(){
+  int x = 1;
+  int y = 1;
+  int s = 0;
+  while (y < 4000000) {
+    y = x + y;
+    x = y - x;
+    if (y % 2 == 0){
+      s = s + y;
+    }
+  }
+  print(s);
+}
